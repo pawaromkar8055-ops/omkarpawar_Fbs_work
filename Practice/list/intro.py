f372d1939c727f1a303fdde8e40077bc1110486b
OmkarPawar <pawaromkar8055@gmail.com>
2026-09-21 21:25:00 +0530

    Add files via upload

---
+++ Practice/list/intro.py
@@ -0,0 +1,14 @@
+#1. denoted by[]
+li=[10,20,30,40,3.14,'abc',10,10,20]
+
+#2.Heterogeneous
+
+#3.ordered
+
+#4.Mutable
+#li[0]=50
+
+#5. Duplicate elements are allowed
+
+print(type(li))
+print(li)
---
+++ Practice/list/list_methods.py
@@ -0,0 +1,16 @@
+li=[10,30,20,40]
+
+# li.append(50)
+# li.clear
+# li2=li.copy
+# #research on shallow copy and deep copy
+# print(li.count(20))
+# li.extend([60,70,80])
+# print(li.index(40))
+# li.insert(2,50)
+# li.pop(1)
+# li.remove(20)
+# li.reverse()
+# li.sort()
+# li.sort(reverse=True)
+print(li)
