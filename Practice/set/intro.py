4f22d635f438ceb6bd3a60433ca827790abd1476
OmkarPawar <pawaromkar8055@gmail.com>
2026-09-25 09:59:38 +0530

    Add files via upload

---
+++ Practice/set/intro.py
@@ -0,0 +1,15 @@
+#1.{} , blank set:str()
+s1={10,20,30,40,'abcdde',10}
+
+#2. heterogeneous
+
+#3. unordered 
+
+#4. mutable , not editable
+
+#5. unique
+#s1.add(70)
+# s1[0]=80 #raise  error
+
+print(type(s1))
+# print(s1)
---
+++ Practice/set/set_methods.py
@@ -0,0 +1,23 @@
+s1={10,20,30,40}
+s2={30,40,50,60}
+s3={50,60}
+s4={50,60}
+
+# s1.add(70) #add single element
+# s1.clear()
+# s5=s1.copy()
+# print(s1.difference(s2))
+# s1.difference_update(s2)
+# s1.discard(50) #element not avilable then print set only and not give error
+# print(s1.intersection(s2)) #give common element of 2 set
+# s1.intersection_update(s2) #give common element of 2 set 
+# print(s1.isdisjoint(s3))
+# print(s3.issubset(s2))
+# print(s2.issuperset(s3))
+# s1.pop() #remove any element from set
+# s1.remove(40) #remove given element from set or element not present then give error
+# print(s1.symmetric_difference(s2))
+# s1.symmetric_difference_update(s2)
+# print(s1.union(s2))
+s1.update({70,80,90}) #add multiple element
+print(s1)
